81716a41f910f02d505a127cb9df076d428d491b
Terrynyam <terrynyamz93@gmail.com>
2022-09-09 12:01:45 +0200

    my code

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        int i;
+        int[] a = {10,11,12,13,14,15,17};
+
+        for (i = a.length-1; i >=0; i--)
+           {
+               System.out.println(a [i]);
+           }
+
+    }
+}
